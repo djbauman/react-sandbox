b562cc9f653156a6f0f3f1c746521ce9ee61139f
djbauman <danieljbauman@gmail.com>
2020-06-29 17:41:57 -0500

    routes setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import {Router, Route} from "react-router";
+import createBrowserHistory from "../history"
+
 import { Root } from "./Root";
 import { Home } from "./Home";
+import { User } from "./User";
 
 export class App extends React.Component {
-
   render() {
     return(
-      <Root>
-        <Home></Home>
-      </Root>
+      <Router history={createBrowserHistory}>
+        <Route path={"/"} component={Root}>
+          <Route path={"user"} component={User} />
+          <Route path={"home"} component={Home} />
+        </Route>
+        <Route path={"home"} component={Home} />
+      </Router>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,22 @@ import React from "react";
 
 export const Header = (props) => {
   return(
-    <nav className="navbar navbar-default">
-      <div className="container">
-        <div className="navbar-header">
-          <ul className="nav navbar-nav">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">User</a></li>
-          </ul>
-        </div>
-      </div>
+    // <nav className="navbar navbar-default">
+    //   <div className="container">
+    //     <div className="navbar-header">
+    //       <ul className="nav navbar-nav">
+    //         <li><a href="#">Home</a></li>
+    //         <li><a href="#">User</a></li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
+
+    <nav class="navbar navbar-light bg-light">
+      <ul className="nav navbar-nav">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">User</a></li>
+      </ul> 
     </nav>
   );
 };
