c8808d33dac92f86697e316976e4619a4d8ad60f
djbauman <danieljbauman@gmail.com>
2020-07-20 19:18:13 -0500

    parameterize handleClose

--- src/components/NewButton.js
+++ src/components/NewButton.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { Button, ButtonGroup, Dropdown } from "react-bootstrap";
 import MyModal1 from './MyModal1';
-// import MyModal2 from './MyModal2';
-// import MyModal3 from './MyModal3';
+import MyModal2 from './MyModal2';
+import MyModal3 from './MyModal3';
 
 export class NewButton extends Component {
   constructor(props) {
@@ -23,23 +23,13 @@ export class NewButton extends Component {
     this.setState(obj);
   }
 
-  handleClose1() {
+  handleClose(i) {
     console.log("Clicked handleClose...");
-    this.setState({
-      show_m1: false,
-    })
-  }
-  handleClose2() {
-    console.log("Clicked handleClose...");
-    this.setState({
-      show_m2: false,
-    })
-  }
-  handleClose3() {
-    console.log("Clicked handleClose...");
-    this.setState({
-      show_m3: false,
-    })
+    console.log("i is: " + i);
+    var obj = {}
+    var key = "show_m" + i
+    obj[key] = false;
+    this.setState(obj);
   }
 
   render() {
@@ -50,18 +40,16 @@ export class NewButton extends Component {
           <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
           <Dropdown.Menu>
             <Dropdown.Item><Link onClick={() => this.onClickNew(1)}>New thing 1</Link></Dropdown.Item>
-            <Dropdown.Item><Link to="/action-2">New thing 2</Link></Dropdown.Item>
+            <Dropdown.Item><Link onClick={() => this.onClickNew(2)}>New thing 2</Link></Dropdown.Item>
             <Dropdown.Divider />
-            <Dropdown.Item><Link to="/action-3">New thing 3</Link></Dropdown.Item>
+            <Dropdown.Item><Link onClick={() => this.onClickNew(3)}>New thing 3</Link></Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
 
-        <MyModal1 show_m1={this.state.show_m1} handleClose={() => this.handleClose1()} />
-        {/* <MyModal2 show={this.state.show} handleClose={() => this.handleClose()} />
-        <MyModal3 show={this.state.show} handleClose={() => this.handleClose()} /> */}
-
-
-
+        <MyModal1 show_m1={this.state.show_m1} handleClose={() => this.handleClose(1)} />
+        <MyModal2 show_m2={this.state.show_m2} handleClose={() => this.handleClose(2)} />
+        <MyModal3 show_m3={this.state.show_m3} handleClose={() => this.handleClose(3)} />
+        
 
       </div>
     )
